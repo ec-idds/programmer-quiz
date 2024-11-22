ef625144cce1d15f7af36b2c2ccbd99c72383659
isazfranz <ifranzese14@gmail.com>
2024-11-22 14:11:02 -0500

    serves question page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const port = 3000
-
-var http = require('http');
+var url = require('url');
 var fs = require('fs');
+var http = require('http');
+
 
 app.get('/', (req, res) => {
   fs.readFile('html/startpage.html', function(err, data) {
@@ -11,8 +13,21 @@ app.get('/', (req, res) => {
     res.write(data);
     return res.end();
   });
-})
+});
+
+app.get('/q', (req, res) => {
+  var q = url.parse(req.url, true);
+  fs.readFile('html/questionpage.html', function(err, data){
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  })
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
