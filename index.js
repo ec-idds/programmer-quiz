2c34569e11e70920be960b57121934e4bcd96243
elijah <elijahbaron12@gmail.com>
2024-11-23 20:09:36 -0500

    Serving Results Page
    Fixes issue #8 we can now serve results page

--- index.js
+++ index.js
@@ -28,6 +28,19 @@ app.get("/q", (req, res) => {
   });
 });
 
+app.get('/r', (req, res) => {
+  var q = url.parse(req.url, true);
+  fs.readFile('html/resultspage.html', function(err, data){
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  })
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
