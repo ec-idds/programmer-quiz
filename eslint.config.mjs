2a82c82e0c0f25e71308fa0c50e7b9106c666926
Mark Sherman <shermanm@emmanuel.edu>
2024-11-24 14:32:58 -0500

    Add eslint, prettier, vscode and github actions

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+];
