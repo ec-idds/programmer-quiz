48d26d02f1434167a471fb6781e5f6b689733d68
elijah <elijahbaron12@gmail.com>
2024-12-11 17:27:05 -0500

    Working start button
    made changes to how out JS file reads in files
    using path.join, while trying to maintain our
    initial structure as to not mess with handle bars

--- index.js
+++ index.js
@@ -2,62 +2,65 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const fs = require("fs");
+const path = require("path");
 const Handlebars = require("handlebars");
 
 let questionData;
 try {
-  const jsonData = fs.readFileSync("question_data.json", "utf8");
+  const jsonData = fs.readFileSync(
+    path.join(__dirname, "question_data.json"),
+    "utf8",
+  );
   questionData = JSON.parse(jsonData);
 } catch (err) {
   console.error("Error reading or parsing question_data.json:", err);
-  process.exit(1); // Exit the server if the JSON file is not accessible
+  process.exit(1);
 }
 
-app.use(express.static("html"));
+app.use(express.static(path.join(__dirname, "html")));
 
 app.get("/", (req, res) => {
-  fs.readFile("html/startpage.html", function (err, data) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    return res.end();
-  });
+  res.sendFile(path.join(__dirname, "html", "startpage.html"));
 });
 
 app.get("/q", (req, res) => {
-  fs.readFile("html/questionpage.html", function (err, htmlData) {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      return res.end("404 Not Found");
-    }
+  fs.readFile(
+    path.join(__dirname, "html", "questionpage.html"),
+    "utf8",
+    (err, htmlData) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
 
-    const questionTemplate = Handlebars.compile(htmlData.toString());
-    const questionNumber = parseInt(req.query.p, 10);
+      const questionTemplate = Handlebars.compile(htmlData.toString());
+      const questionNumber = parseInt(req.query.p, 10);
 
-    // Find the question by ID
-    const question = questionData.find((q) => q.id === questionNumber);
+      // Find the question by ID
+      const question = questionData.find((q) => q.id === questionNumber);
 
-    if (!question) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      return res.end("Question not found");
-    }
+      if (!question) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("Question not found");
+      }
 
-    // Replace placeholders with data
-    const replacements = {
-      qno: questionNumber,
-      question: question.question,
-      answers: question.answers.map((answer) => answer.text), // Pass array of answer texts
-    };
+      const replacements = {
+        qno: questionNumber,
+        question: question.question,
+        answers: question.answers.map((answer) => answer.text),
+      };
 
-    const renderedHtml = questionTemplate(replacements);
+      const renderedHtml = questionTemplate(replacements);
 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(renderedHtml);
-    return res.end();
-  });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(renderedHtml);
+      return res.end();
+    },
+  );
 });
 
 app.get("/r", (req, res) => {
-  fs.readFile("html/resultspage.html", function (err, data) {
+  fs.readFile(path.join(__dirname, "html", "resultspage.html"), (err, data) => {
     if (err) {
       res.writeHead(404, { "Content-Type": "text/html" });
       return res.end("404 Not Found");
